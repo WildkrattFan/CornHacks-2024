551bd986485ecd03942c97b26bf49f3760ccb253
Leo Martin <lmartin32@huskers.unl.edu>
2024-02-17 14:42:10 -0600

    new dice

--- Cornhacks2024/dice.java
+++ Cornhacks2024/dice.java
@@ -3,36 +3,6 @@ package Cornhacks2024;
 import java.util.Random;
 
 public class Dice {
-    private String diceType;
-    private int numSides;
-    private String diceDifficulty;
-    private Random random;
-
-    public Dice(String diceType, int numSides, String diceDifficulty, Random random) {
-        this.numSides = numSides;
-        this.diceType = diceType;
-        this.diceDifficulty = diceDifficulty;
-        this.random = random;
-    }
-
-    public String getDiceType() {
-        return this.diceType;
-    }
-
-    public String getDiceDifficulty() {
-        return this.diceDifficulty;
-    }
-
-    public int getNumSides() {
-        return this.numSides;
-    }
-
-    public String toString() {
-        return ("Name: " + this.diceType + " NumSides: " + this.numSides + " Type: " + this.diceType + " Difficulty: "
-                + this.diceDifficulty);
-
-    }
-
     public int rollEasyChoice() {
         return random.nextInt(5) + 1;
 
@@ -45,5 +15,6 @@ public class Dice {
     public int rollHardChoice() {
         return random.nextInt(15) + 1;
     }
-
 }
+
+

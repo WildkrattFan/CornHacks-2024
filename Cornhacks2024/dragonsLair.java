b4c4b926f54d433c0bb5aaa7bc45c32bb0090d21
owenmiester05 <ofaddison@gmail.com>
2024-02-17 14:46:48 -0600

    Update dragonsLair.java

--- Cornhacks2024/dragonsLair.java
+++ Cornhacks2024/dragonsLair.java
@@ -1,12 +1,24 @@
 package Cornhacks2024;
 import java.util.Scanner;
 public class dragonsLair {
+    public static void readyToDieMessage(){
+        
+    }
     public static void dragonsLair() {
         System.out.println("Welcome to the Dragon's Lair \n Are you ready to die?");
         System.out.println("Type 'yes' or 'no' ...")
-        Scanner input = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (input.equals("yes") == False) {
+            String input = scanner.nextLine();
+            System.out.println("SORRY WRONG ANSWER!!! \n Lets try that again...");
+            System.out.println("Are you ready to die")
+            System.out.println("Type yes or no");
+            }
+            
+        }
     }
-}
+
 
 
         

d0a74334d9edffb87fa1c3c81357cad4a5844337
Leo Martin <lmartin32@huskers.unl.edu>
2024-02-17 12:37:02 -0600

    new objects

---
+++ Cornhacks 2024/character.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class player{
+    private String name;
+}
+public player(String name){
+    this.name = name;
+}
+public String getName(){
+    return this.name;
+}
+public setName(String name){
+    this.name = name;
+}
---
+++ Cornhacks 2024/die.java
@@ -0,0 +1,26 @@
+public class dice{
+    private String name;
+    private int numSides;
+    
+    public dice(String name, int numSides){
+        this.name = name;
+        this.numSides = numSides;
+    }
+    public getName(){
+        return this.name;s
+    }
+    public getNumSides(){
+        return this.numSides;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setNumSides(int numSides){
+        this.numSides = numSides;
+    }
+    public toString toString(){
+        return ("Name: " + this.name + " NumSides: " + this.numSide)
+
+    }
+
+}

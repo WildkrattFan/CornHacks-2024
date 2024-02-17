eb7752f3cefdf49b31842aaa1780aa1585c3b186
WildkrattFan <cosmiccollectors131@gmail.com>
2024-02-17 11:38:29 -0600

    blackJackBot created

---
+++ blackJackBot.java
@@ -0,0 +1,7 @@
+import net.dv8tion.jda.core.AccountType;
+
+
+public class statisticsBot {
+    public static void main(String[] args) {
+        // Create a new instance of the bot
+        JDA bot = new JDABuilder(AccountType.BOT)

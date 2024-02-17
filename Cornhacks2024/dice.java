5a8c621e2cc621649de8c39d6eb036a916c62ad6
Leo Martin <lmartin32@huskers.unl.edu>
2024-02-17 16:10:39 -0600

    Update dice.java

--- Cornhacks2024/dice.java
+++ Cornhacks2024/dice.java
@@ -5,15 +5,15 @@ import java.util.Random;
 public class dice {
     private Random random = new Random();
 
-    public int rollEasyChoice() {
+    public static int rollEasyChoice() {
         return random.nextInt(5) + 1;
     }
 
-    public int rollMediumChoice() {
+    public static int rollMediumChoice() {
         return random.nextInt(10) + 1;
     }
 
-    public int rollHardChoice() {
+    public static int rollHardChoice() {
         return random.nextInt(15) + 1;
     }
 }

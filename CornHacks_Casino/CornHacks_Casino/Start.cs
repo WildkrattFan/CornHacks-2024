1513def22101cc5cb7c7eac4e802c111bab20992
86xenon <zoekerchal@gmail.com>
2024-02-17 15:37:45 -0600

    Set up random to determine name

--- CornHacks_Casino/CornHacks_Casino/Start.cs
+++ CornHacks_Casino/CornHacks_Casino/Start.cs
@@ -3,8 +3,18 @@ namespace CornHacks_Casino
     
     public partial class Start : Form
     {
+        Random random = new Random();
         public string name = "";
         public int diceNum;
+        string[] location = new string[] { "Dragons Lair", "South Mountains", "East Village", "Goblin Hut", "Gold Hoard", "South Mountains" };
+
+
+        public int Random(int max)
+        {
+            int randomNum = random.Next(1, (max + 1));
+            return randomNum;
+            
+        }
 
         public Start()
         {
@@ -40,7 +50,7 @@ namespace CornHacks_Casino
 
         private void Next0_Click(object sender, EventArgs e)
         {
-            diceNum = 1;
+            diceNum = Random(6);
             Dice_Value.Text = diceNum.ToString();
             Dice_Value.Show();
             Dice.Show();
@@ -58,13 +68,22 @@ namespace CornHacks_Casino
                 name = "Wobbleknob";
                 finalName.Text = "You rolled a " + diceNum + "! Your name is Wobbleknob ";
             }
-            
+            Next2.Show();
+            Roll_Again.Show();
+            nameTextBox.Hide();
             finalName.Show();
         }
 
         private void finalName_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Next2_Click(object sender, EventArgs e)
+        {
+            diceNum = Random(6);
+            Dice_Value.Text = diceNum.ToString();
+            finalName.Text = "You rolled a " + diceNum + "! Click next to see where you go!";
         }
     }
 }

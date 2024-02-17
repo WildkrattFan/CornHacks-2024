c9b66d2f019518b58aef3252c24afb170c36083d
owenmiester05 <ofaddison@gmail.com>
2024-02-17 16:48:18 -0600

    Update dragonsLair.java

--- Cornhacks2024/dragonsLair.java
+++ Cornhacks2024/dragonsLair.java
@@ -1,6 +1,7 @@
 package Cornhacks2024;
 
 import java.util.Scanner;
+import Dice;
 
 public class dragonsLair {
     Dice dice = new Dice();
@@ -16,23 +17,39 @@ public class dragonsLair {
         System.out.println("Will you dodge the Dragon?");
         System.out.println("Type 'yes' or 'no'");
         runTrue = scanner.nextLine();
-        if runTrue.equals("yes") {
-            roll = dice.rollMediumChoice();
-            if (roll >= 6) {
-                System.out.println("You rolled a " + roll);
-                System.out.println("Congrats, you have dodged the Dragon");
-                southMountains(player);
-            } else {
-                System.out.println("You rolled a " + roll);
-                System.out.println("The Dragon hits you with a wrath of envious fire \n you die");
-                System.exit(0);
+            while (runTrue.equals("yes") == true || runTrue.equals("no") == true) {
+                if runTrue.equals("yes") {
+                    roll = dice.rollMediumChoice();
+                    if (roll >= 6) {
+                        System.out.println("You rolled a " + roll);
+                        System.out.println("Congrats, you have dodged the Dragon");
+                        southMountains(player);
+                    } else {
+                        System.out.println("You rolled a " + roll);
+                        System.out.println("The Dragon hits you with a wrath of envious fire \n you die");
+                        System.exit(0);
+                    }
+                } else if runTrue.equals("no") {
+                    System.out.println("Uh oh, you died");
+                    System.exit(0);
+                } else {
+                    System.out.println("Enter a valid input");
+                    runTrue = scanner.nextLine();
+                }
             }
         }
-    }
+    public static void fistFight(int roll){
+            System.out.println("You rolled a " + roll);
+            System.out.println("You decide to fist fight the dragon");
+            roll2 = 20;
+            System.out.println("You rolled a " + roll2);
+            System.out.println("You punch the dragon straight in its nose, \n you are then engulfed in a massive flame and die");
+            System.exit(0);  
+        }
     public static void readyToDieMessage(){
         System.out.println("Are you ready to die");
         System.out.println("Type 'yes' or 'no'");
-    } 
+        } 
     public static void dragonInLair(){
         System.out.println("Dragon in Lair");
         System.out.println("The Dragon sees you how would you like to react");
@@ -41,12 +58,16 @@ public class dragonsLair {
         roll = dice.rollHardChoice();
         if (roll == 16){
             magicWizardHand();
-        } else if (roll >= 12){
+        } else if (roll >= 8){
             runAway();
+        } else {
+            fistFight()
         }
     }
     public static void dragonNotInLair(){
-        System.out.println("You enter ");
+        System.out.println("You entered the Dragon's Lair and there is no dragon, so you leave");
+        southMountains(player); 
+        }
 
     public static void dragonsLair(player) {
         System.out.println("Welcome to the Dragon's Lair");
@@ -67,7 +88,6 @@ public class dragonsLair {
             
         }
     }
-}
 
 
 

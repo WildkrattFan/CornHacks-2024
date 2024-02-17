d596d078256fa7527e483613f1ee44a8cc882ca8
Leo Martin <lmartin32@huskers.unl.edu>
2024-02-17 12:58:19 -0600

    object updates

--- Cornhacks 2024/character.java
+++ Cornhacks 2024/character.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 public class player{
     private String playerName;
+    private String playerAttribute;
 }
 public player(String playerName){
     this.playerName = playerName;
@@ -8,6 +9,15 @@ public player(String playerName){
 public String getName(){
     return this.playerName;
 }
+public String getAttribute(){
+    return this.playerAttribute;
+}
 public setName(String playerName){
     this.playerName = playerName;
+}
+public setAttribute(String playerAttribute){
+    this.playerAttribute = playerAttribute;
+}
+public player toString(){
+    return ("Name: " + this.playerName + " Attribute: " + this.playerAttribute);
 }

f4778c77954b88be9c75e77fc1f07dfebe82e75b
WildkrattFan <cosmiccollectors131@gmail.com>
2024-02-17 13:12:20 -0600

    Fixing formatting

--- Cornhacks2024/textAdventureBase.java
+++ Cornhacks2024/textAdventureBase.java
@@ -7,7 +7,6 @@ public class textAdventureBase {
 
     public static void main(String[] args) {
         // Start the game
-        List<Item> inventory = new ArrayList<>();
         
         System.out.println("Welcome to the amazing world of Valdoria brave adentureer!");
         System.out.println("You are about to embark on a journey that will test your wits and courage.");
@@ -45,9 +44,9 @@ public class textAdventureBase {
 }
 
     // Game Area 1
-    public static void gameAeraOne(List<Item> inventory) {
+    public static void gameAeraOne() {
 
-        System.out.println("Wizzard: Greetings Traveler! Welcome to my humble abode. What should I call you?");
+        System.out.println("Wizard: Greetings Traveler! Welcome to my humble abode. What should I call you?");
         String name = System.console().readLine();
         // User input name
         if (dice.rollMediumChoice() >= 5) {
@@ -55,6 +54,32 @@ public class textAdventureBase {
         } else {
             System.out.println("Wizzard: " + name + " is a terrible name. I will call you Bob.");
             character player = new character("Bob");
+        }
+
+        System.out.println("Wizard: " + player.getName() + " I have a quest for you.");
+        System.out.println(
+                "Wizard: I need you to travel north to the cave of wonders and retrieve the magical gauntlet of power!");
+        System.out.println("Wizard: Are you up to the task?");
+        // Player input yes or no
+        String input = System.console().readLine();
+
+        if (input.equals("yes")) {
+            if (dice.rollMediumChoice() >= 5) {
+                System.out.println("Wizard: Excellent! I knew I could count on you " + player.getName() + ".");
+                System.out.println("Wizard: Here is a map to the cave. Good luck!");
+                gameAreaTwo(player);
+            } else {
+                System.out.println("Wizard: I see. Well, I guess I will have to find someone else to do it.");
+                System.out.println("Wizard: Goodbye " + player.getName() + ".");
+                System.exit(0);
+            }
+            gameAreaTwo(player);
+        } else if (input.equals("no")) {
+            System.out.println("Wizard: I see. Well, I guess I will have to find someone else to do it.");
+            System.out.println("Wizard: Goodbye " + player.getName() + ".");
+            System.exit(0);
+        } else {
+            System.out.println("Wizard: I'm sorry, I didn't catch that. Please type 'yes' or 'no'.");
         }
 
     }

7dd994c34add2032556f062de4d9e26045acea69
WildkrattFan <cosmiccollectors131@gmail.com>
2024-02-17 16:45:06 -0600

    southMountains final I think

--- Cornhacks2024/southMountains.java
+++ Cornhacks2024/southMountains.java
@@ -1,7 +1,7 @@
 package Cornhacks2024;
 
 //TODO: Cave of wonders() -- Success
-//TODO: Mugged() -- Failure
+//TODO: Mugged() -- Failure --- done
 
 public class southMountains {
     public static void exploreSouthMountains(character player) {
@@ -31,8 +31,53 @@ public class southMountains {
     }
 
     public static void rushDownMountain(character player) {
+        dice diceObject = new dice();
+        int roll = diceObject.rollMediumChoice();
+        dice diceObject2 = new dice();
+        int roll2 = diceObject2.rollMediumChoice();
         System.out.println("You sprint down the mountain with the speed of the fastest horses.");
         System.out.println("You feel the wind in your hair and the sun on your face.");
+        System.out.println("You come to a stop at the base of the mountain, feeling invigorated.");
+        if (roll >= 5) {
+            System.out.println("You rolled a " + roll);
+            System.out.println("You suddenly spot something in the distance.");
+            System.out.println("Would you like to investigate?");
+            String input = System.console().readLine();
+            if (input.equals("yes")) {
+
+                if (roll2 >= 5) {
+                    System.out.println("You rolled a " + roll2);
+                    System.out.println("You find a cave in the distance.");
+                    System.out.println("You feel a sense of wonder and excitement.");
+                    System.out.println("You feel as though you could take on the world.");
+                    caveOfWonders(player);
+                } else {
+                    System.out.println("You turn back and leave the mountains.");
+                    System.out.println("You feel as though you have missed out on something great.");
+                    System.exit(1);
+                }
+
+            } else {
+                if (roll2 < 5) {
+                    System.out.println("You rolled a " + roll2 + " and have no choice but to investigate.");
+                    System.out.println("You find a cave in the distance.");
+                    System.out.println("You feel a sense of wonder and excitement.");
+                    System.out.println("You feel as though you could take on the world.");
+                    caveOfWonders(player);
+                } else {
+                    System.out.println("You turn back and leave the mountains.");
+                    System.out.println("You are suddenly attacked by a group of bandits!");
+                    mugged(player);
+                }
+            }
+
+        } else {
+            System.out.println("You rolled a " + roll);
+            System.out.println("You feel a sense of dread and despair.");
+            System.out.println("You feel as though you have failed.");
+            depressed(player);
+        }
+
     }
 
     public static void depressed(character player) {
@@ -74,5 +119,42 @@ public class southMountains {
 
         }
 
+    }
+
+    public static void mugged(character player) {
+        System.out.println("You are outnumbered and outmatched.");
+        System.out.println("Would you like to fight or surrender?");
+        String input = System.console().readLine();
+
+        if (input.equals("fight")) {
+            dice diceObject = new dice();
+            int roll = diceObject.rollHardChoice();
+            if (roll >= 10) {
+                System.out.println("You rolled a " + roll);
+                System.out.println("You are able to fight off the bandits and escape.");
+                System.out.println("You leave the mountains and continue on your journey.");
+                System.out.println("Along the way you run into the Cave of Wonders!");
+                caveOfWonders(player);
+            } else {
+                System.out.println("You rolled a " + roll);
+                System.out.println("You are quickly overpowered and killed.");
+                System.exit(1);
+            }
+        } else {
+            System.out.println("You decide to surrender!");
+            dice diceObject = new dice();
+            int roll = diceObject.rollHardChoice();
+            if (roll >= 10) {
+                System.out.println("You rolled a " + roll);
+                System.out.println("The bandits accept your surrender and take you as their servant.");
+                System.out.println("You are forced to work for them for the rest of your life.");
+                System.exit(1);
+            } else {
+                System.out.println("You rolled a " + roll);
+                System.out.println("The bandits do not accept your surrender.");
+                System.out.println("You are quickly overpowered and killed.");
+                System.exit(1);
+            }
+        }
     }
 }
